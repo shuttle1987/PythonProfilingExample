baba4a209b1931625dcf531b6da57e7ac45f8a89
Janis Lesinskis <janis@lesinskis.com>
2020-02-18 09:28:11 +1100

    Improve comments

--- slow.py
+++ slow.py
@@ -2,10 +2,11 @@ from sorting import bogo
 from random import shuffle
 
 if __name__ == '__main__':
-    ordered = [1, 2, 3, 4, 5]
-
+    # See perf of sorting and already ordered collection
+    ordered = list(range(10))
     bogo(ordered)
 
+    # See perf of sorting random ordered collection
     ints = list(range(10))
     shuffle(ints)
     bogo(ints)
